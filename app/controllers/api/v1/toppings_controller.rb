d2d884f21756fbdc7a07503846fc65406608ecdc
Scott Rowe <a.scott.rowe@gmail.com>
2017-05-30 23:11:25 -0400

    Add topping create route

--- app/controllers/api/v1/toppings_controller.rb
+++ app/controllers/api/v1/toppings_controller.rb
@@ -7,6 +7,19 @@ module Api
         render json: @toppings
       end
 
+      def create
+        topp = Topping.new topping_params
+        if topp.save
+          render json: {success: 'yeah success'}
+        else
+          render json: {error: 'ya bozo'}
+        end
+      end
+
+      def topping_params
+        params.permit(:name, :extra_cost )
+      end
+
     end
   end
 end
