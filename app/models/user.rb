44d1b1ca860affcad1c225166c7a3bb1cb2144de
Scott Rowe <a.scott.rowe@gmail.com>
2017-05-30 22:21:41 -0400

    Add roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  include RoleModel
+  roles_attribute :roles_mask
+  roles :admin
+
   validates :email, presence: true, uniqueness: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530032914) do
+ActiveRecord::Schema.define(version: 20170531020751) do
 
   create_table "toppings", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170530032914) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "roles_mask"
   end
 
 end
